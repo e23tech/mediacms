12f0a665db8560cc9813eaeb94497d847333db9a
Dong Chen <cdcchen@gmail.com>
2012-03-12 20:09:03 +0800

    e

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -1,11 +1,14 @@
 <?php
 class ApiController extends Controller
 {
+    
     public function actionIndex()
     {
-        header('content-type', 'application/json');
+        header('content-type:application/json; charset=' . app()->charset);
+        
         $api = new AppApi($_REQUEST);
         $api->run();
+        exit(0);
     }
     
     public function actionJsonp()
