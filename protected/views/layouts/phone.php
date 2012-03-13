99272491df5b807efa27c47488bcd029355d8c33
Dong Chen <cdcchen@gmail.com>
2012-03-13 21:30:59 +0800

    w

--- protected/views/layouts/phone.php
+++ protected/views/layouts/phone.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="content-type" content="text/html; charset=<?php echo app()->charset;?>" />
-    <title><?php echo $title;?></title>
-    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no" />
-    <meta content="yes" name="apple-mobile-web-app-capable" />
-    <meta content="black" name="apple-mobile-web-app-status-bar-style" />
-    <meta content="telephone=no" name="format-detection" />
+<meta http-equiv="content-type" content="text/html; charset=<?php echo app()->charset;?>" />
+<title><?php echo $title;?></title>
+<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no" />
+<meta content="yes" name="apple-mobile-web-app-capable" />
+<meta content="black" name="apple-mobile-web-app-status-bar-style" />
+<meta content="telephone=no" name="format-detection" />
+<style type="text/css">
+img {max-width:920px;}
+</style>
 </head>
 <body>
 <?php echo $content;?>
