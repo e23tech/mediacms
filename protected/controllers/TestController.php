93b36fe025d27e2309fe4e8970c38098832fed1f
Dong Chen <cdcchen@gmail.com>
2012-03-14 16:51:52 +0800

    ww

--- protected/controllers/TestController.php
+++ protected/controllers/TestController.php
@@ -3,6 +3,25 @@ class TestController extends Controller
 {
     public function actionTest1()
     {
+        $str = <<<EOD
+ <div class="sn-container">
+            <p id="login-info" class="sn-login-info">
+                                                             <script>
+	                            if (TB && TB.Global&& TB.Global.writeLoginInfo) {
+                                      TB.Global.writeLoginInfo({
+                                        isApp: true,
+                                        passCookie:true
+                                   });
+                                 }
+                        </script>
+                                                  </p>
+EOD;
+        $pattern = '/\s*<script.*?writeLoginInfo.*?<\/script>\s*/is';
+        $new = preg_replace($pattern, '123', $str);
+        
+        var_dump($new);
+        exit;
+        
 //         var_dump(app()->session->isStarted);
 //         session_start();
         var_dump(app()->session);
