38f3bc10601cf5be63f4b0e43069ec52b30b997f
Dong Chen <cdcchen@gmail.com>
2012-03-14 09:40:25 +0800

    ee

--- protected/apis/Api_Special.php
+++ protected/apis/Api_Special.php
@@ -8,6 +8,10 @@ class Api_Special extends ApiBase
             ->order(array('create_time desc', 'id desc'))
             ->limit(20);
         $rows = $cmd->queryAll();
+        
+        foreach ($rows as $key => $row) {
+            $rows[$key]['create_time_text'] = date(param('formatShortDateTime'), $row['create_time']);
+        }
         return $rows;
     }
 }
